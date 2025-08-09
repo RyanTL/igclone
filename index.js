5107561b098019e903b1eb632eb7b3e0eac3123a
RyanTL <113138001+RyanTL@users.noreply.github.com>
2025-08-08 23:03:35 -0400

    IG clone with dinamic data using js

--- index.js
+++ index.js
@@ -28,3 +28,44 @@ const posts = [
     }
 ]
 
+
+
+
+    const container = document.getElementById("posts-container")
+    let posthtml = ""
+
+    for (let i = 0; i < posts.length; i++){
+        const post = posts[i]
+        posthtml += `
+
+            <div class = "user-info">
+            <img src="${post.avatar}" class="user-avatar">
+            <div class="name-location">
+            <h3>${post.name}</h3>
+            <p>${post.location}</p>
+            </div>
+        </div>
+
+        <div class="post-imxage">
+            <img src="${post.post}" class="post-image">
+        </div>
+
+        <div class="interaction-container">
+            <div class="interaction-buttons">
+            <img src="images/icon-heart.png" class="int-icon">
+            <img src="images/icon-comment.png" class="int-icon">
+            <img src="images/icon-dm.png" class="int-icon">
+            </div>
+            <p class="bold">${post.likes} likes</p>
+            <p class="comments"> <span class="bold">${post.username}</span> ${post.comment}</p>
+        </div>        
+        
+        `;
+    }
+
+    container.innerHTML = posthtml;
+
+
+
+
+
